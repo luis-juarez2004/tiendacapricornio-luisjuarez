819a99619145ac2abaec8715b773656dbe69268f
Luis Juarez <chanjuarez8@gmail.com>
2022-05-15 18:42:01 -0300

    storage y JSON

--- js/main.js
+++ js/main.js
@@ -1,91 +1,4 @@
-//alert()
-
-/* const MIN_POWER = 30;
-const MAX_POWER = 50;
-
-let vidaThanos = 100;
-let vidaIronman = 100;
-
-let round = 1;
-
-const calcularGolpe = () => Math.ceil(Math.random()* (MAX_POWER - MIN_POWER) + MIN_POWER)
-
-const tieneEnergia = () => vidaThanos > 0 &&  vidaIronman > 0
-
-
-const ThanosSigueEnPie = () => vidaIronman < 0
-
-
-while(tieneEnergia()){
-
-console.log("Round " + round)
-
-let golpeThanos = calcularGolpe();
-console.log("Golpe Thanos" + golpeThanos)
-
-
-let golpeIronman = calcularGolpe();
-console.log("Golpe Iron man" + golpeIronman)
-
-if(golpeIronman>golpeThanos){
-    vidaThanos -=golpeIronman
-}else {
-    vidaIronman -=golpeThanos;
-}
-
-if(golpeIronman>20 && vidaIronman < 100){
-    vidaIronman +=5;
-}
-
-if(golpeThanos>20 && vidaThanos){
-    vidaThanos +=5;
-}
-
-console.log("Vida Iron man" + vidaIronman)
-console.log("Vida Thanos" + vidaThanos)
-
-round +=1;
-}
-
-if(ThanosSigueEnPie()){
-    console.log("Gano Thanos")
-}else {
-    console.log("Gano Iron man")
-}   */
-
-/* let producto;
-let desc = 0 ;
-let cant = 0 ;
-let precio = 0 ;
-let listaProd = [] ;
-let precioFinal = 0; 
-
-class Producto {
-    constructor (cant, producto, precio){
-        this.cant = cant;
-        this.producto = producto;
-        this.precio = precio;
-    }
-}
-
-const agregarProd = () => {
-    cant = parseInt(prompt("ingrese la cantidad de productos que desea comprar"));
-    producto = (prompt("ingrese el nombre del producto"));
-    precio = parseInt(prompt("ingrese el precio del producto"));
-    let articulo = new Producto (cant, producto, precio);
-    listaProd.push(articulo);
-
-
-for (let articulo of listaProd){
-    articulo.precio *= cant; 
-    alert(`Usted a agregado ${articulo.cant} ${articulo.producto} al carrito y el precio total es $${articulo.precio}`)
-}
-}
-
-agregarProd(); */
-
-
-let boton1 = document.querySelector("#boton1");
+/* let boton1 = document.querySelector("#boton1");
 
 boton1.addEventListener("click", agregado);
 
@@ -99,9 +12,55 @@ boton3.addEventListener("click", agregado);
 
 function agregado (agr) {
     agr.preventDefault();
-    alert("producto agregado")
 
+    console.log("producto agregado");
 
-    console.log("producto agregado")
+    let carrito = [];
+
+    const agregar = {
+        id: Date.now(),
+        texto: boton1, 
+        texto2: boton2,
+        texto3: boton3,
+    }
+
+carrito.push(agregar)
+
+console.log(carrito);
+} */
+
+const botones = document.querySelectorAll('.addCarrito');
+
+botones.forEach(agregarCarta => {
+    agregarCarta.addEventListener('click', agregarCartaClick)
+});
+
+const elementoCarta = document.querySelector('.container');
+
+function agregarCartaClick(event) {
+    const button = event.target;
+    
+    const item = button.closest('.card');
+
+    const itemTitle = item.querySelector('.titulo').textContent;
+
+    const itemPrecio = item.querySelector('.precio').textContent;
+
+    const itemImagen = item.querySelector('.imagen').src;
+
+    agregarCarrito(itemTitle, itemPrecio, itemImagen);
+} 
+
+function agregarCarrito (itemTitle, itemPrecio, itemImagen){
+    const element = document.createElement('div');
+    element.setAttribute('class', 'prod');
+    const carro = `
+        <img src=${itemImagen} class="card-img-top imagen">
+        <h5 class="card-title titulo" id="title">${itemTitle}</h5>
+        <p class="card-text precio" id="price">${itemPrecio}</p>`;
+    element.innerHTML = carro
+    elementoCarta.append(element);
+    const strJSON= JSON.stringify(carro);
+    localStorage.setItem("producto", strJSON);
 }
 
---
+++ js/productos.js
@@ -0,0 +1,6 @@
+/* let productos = [
+    {id:1, name:"cocaCola2,25LT", precio:230, img:"../assets/img/cocacola.jpg"},
+    {id:2, name:"yerbaCBSÉ", precio:200, img:"../assets/img/yerbacbse.jpg"},
+    {id:3, name:"azucarLedesma", precio:130, img:"../assets/img/azucarledesma.jpg"},
+]
+ */
