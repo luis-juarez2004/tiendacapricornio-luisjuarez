5e24dca1b7baaafbc1d972fc2f3a742a0c168e57
Luis Juarez <chanjuarez8@gmail.com>
2022-05-28 01:19:40 -0300

    agregado API

--- js/main.js
+++ js/main.js
@@ -35,8 +35,31 @@ function agregarCarrito (itemTitle, itemPrecio, itemImagen){
         title: "Producto agregado",
         text: "Usted agrego un producto al carrito!",
         icon: "success",
+        timer: "1000",
     });
 };
 
+let aplicacion = document.querySelector('.clima')
 
+let noticia = async () => {
+    try {
+        let noti = await fetch("http://api.mediastack.com/v1/news?access_key=842bee835537d45424ff8040552397f7&language=-es&countries=gb,us")
+        let resultado = await noti.json();
+        info = resultado.data;
+        info.forEach( not => {{ 
+        let news = document.createElement("div");
+        news.className = "noticia";
+        news.innerHTML = `
+        <p>${not.title}</p>
+        <div>${not.description}</div>
+        <a href="${not.url}">Noticia Aqui!!!</a>
+        `;
+            aplicacion.appendChild(news);
+                }
+            })
+    }catch (error){
+        console.log(error);
+    }
+}
+noticia()
 
